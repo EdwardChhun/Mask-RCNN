ed9eae57e977b588223a298ffd192836fdf37f00
Nathan Pietrantonio <pietrann@uci.edu>
2024-04-29 10:30:29 -0700

    Add more plotting options
    plost fast false negative
    plot classifier loss
    plot classifier loss from rpn
    print total loss

--- visualization/visualize.py
+++ visualization/visualize.py
@@ -55,9 +55,24 @@ class Visualizer:
     def plot_fast_cls_accuracy(self):
         self._plot_over_iterations(y_axis="fast_rcnn/cls_accuracy",
                                    y_axis_title="Fast RCNN Classifier Accuracy",
-                                   title="Fast RCNN Classifier Accuracy")
+                                   title="Fast RCNN Classifier Accuracy by Iteration")
 
+    def plot_fast_false_negative(self):
+        self._plot_over_iterations(y_axis="fast_rcnn/false_negative",
+                                   y_axis_title="Fast RCNN Classifier False Negatives",
+                                   title="Fast RCNN Classifier False Negatives by Iteration")
 
+    def plot_classifier_loss(self):
+        self._plot_over_iterations(y_axis="loss_cls",
+                                   y_axis_title="Classifier Loss",
+                                   title="Classifier Loss by Iteration")
 
-v = Visualizer("example_metrics.json")
-v.plot_fast_cls_accuracy()
+    def plot_classifier_loss_from_rpn(self):
+        self._plot_over_iterations(y_axis="loss_rpn_cls",
+                                   y_axis_title=" Classifier Loss from RPN",
+                                   title="Classifier Loss from RPN by Iteration")
+
+    def plot_total_loss(self):
+        self._plot_over_iterations(y_axis="total_loss",
+                                   y_axis_title="Classifier Total Loss",
+                                   title="Classifier Total Loss by Iteration")
